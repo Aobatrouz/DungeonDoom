c802d7845b777adad86bdef4fe3be518d572b7c6
Jessica Stafford <aobacreates@gmail.com>
2025-03-17 11:47:29 -0700

    Rebuild Clean Up

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator animator;
+
+    [SerializeField] int HP, maxHP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int animTransSpeed;
+
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+
+
+    float shootTimer;
+
+    Vector3 playerDir;
+    bool playerInRage;
+    private EnemyLoot loot;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        loot = GetComponent<EnemyLoot>();
+        gamemanager.instance.updateGameGoal(1);
+        maxHP = HP;
+        updateenemyUI();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        setAnimLocomotion();
+
+        shootTimer += Time.deltaTime;
+
+        if (playerInRage)
+        {
+            playerDir = gamemanager.instance.player.transform.position - transform.position;
+            agent.SetDestination(gamemanager.instance.player.transform.position);
+
+
+
+            if (shootTimer >= shootRate)
+            {
+                shoot();
+            }
+
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                faceTarget();
+            }
+        }
+    }
+
+    void setAnimLocomotion()
+    {
+        float agentSpeed = agent.velocity.normalized.magnitude;
+        float animSpeedCur = animator.GetFloat("Speed");
+        animator.SetFloat("Speed", Mathf.Lerp(animSpeedCur, agentSpeed, Time.deltaTime * animTransSpeed));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRage = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRage = false;
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        HP -= amount;
+        updateenemyUI();
+        StartCoroutine(flashRed());
+
+
+        agent.SetDestination(gamemanager.instance.player.transform.position);
+
+        if (HP <= 0)
+        {
+
+            gamemanager.instance.updateGameGoal(-1);
+            loot.Die();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
+
+    public void updateenemyUI()
+    {
+        //gamemanager.instance.enemyHPBar.fillAmount = HP / (float)maxHP;
+    }
+}
---
+++ Assets/Scripts/gamemanager.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class gamemanager : MonoBehaviour
+{
+    public static gamemanager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] GameObject menuHUD;
+    [SerializeField] TMP_Text waveNumberText;
+
+    public Image WaveTimer;
+    public Image playerHPBar;
+    public Image enemyHPBar;
+    public GameObject playerDamageScreen;
+    public GameObject player;
+    public PlayerController playerScript;
+
+    public bool isPaused;
+
+    int numEnemies;
+    int goalCount;
+    int waveNum = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+        StartNextWave();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+
+            }
+            else if(menuActive == menuPause)
+            {
+                StateUnpause();
+            }
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        //menuActive = menuPause;
+        //menuHUD.SetActive(false);
+        //menuActive.SetActive(true);
+    }
+
+    public void StateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState  = CursorLockMode.Locked;
+        menuHUD.SetActive(true);
+        menuActive.SetActive(false);
+        menuActive = null;
+
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        goalCount += amount;
+
+        if(goalCount <= 0)
+        {
+            if (waveNum > 4)
+            {
+                // You Win
+                statePause();
+                menuActive = menuWin;
+                menuActive.SetActive(true);
+            }
+            else
+            {
+                // Start Next Wave
+                StartNextWave();
+            }
+        }
+    }
+
+    public void youLose()
+    {
+        statePause();
+        playerDamageScreen.SetActive(false);
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+
+    public void StartNextWave()
+    {
+        waveNum++;
+        waveNumberText.text = "Wave " + waveNum.ToString();
+    }
+}
